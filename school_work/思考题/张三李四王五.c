c5ce3df5976bc7c7031c8edd7aceac6cbf0db28e
木篁酱 <13430690708@163.com>
2025-12-17 20:30:31 +0800

    以谷歌格式修改了代码

--- school_work/思考题/张三李四王五.c
+++ school_work/思考题/张三李四王五.c
@@ -1,52 +1,45 @@
 #include <stdio.h>
-int main()
-{
-    int z3, l4, w5;
-    for (z3 = 0; z3 <= 1; z3++)
-    {
-        for (l4 = 0; l4 <= 1; l4++)
-        {
-            for (w5 = 0; w5 <= 1; w5++)
-            {
-                int condition1 = ((z3 == 1) && (l4 == 0)) || ((z3 == 0) && (l4 == 1));
-                int condition2 = ((l4 == 1) && (w5 == 0)) || ((l4 == 0) && (w5 == 1));
-                int condition3 = ((w5 == 1) && (z3 == 0 && l4 == 0)) || ((w5 == 0) && (z3 == 1 || l4 == 1));
+int main() {
+  int z3, l4, w5;
+  for (z3 = 0; z3 <= 1; z3++) {
+    for (l4 = 0; l4 <= 1; l4++) {
+      for (w5 = 0; w5 <= 1; w5++) {
+        int condition1 = ((z3 == 1) && (l4 == 0)) || ((z3 == 0) && (l4 == 1));
+        int condition2 = ((l4 == 1) && (w5 == 0)) || ((l4 == 0) && (w5 == 1));
+        int condition3 = ((w5 == 1) && (z3 == 0 && l4 == 0)) ||
+                         ((w5 == 0) && (z3 == 1 || l4 == 1));
 
-                if (condition1 == 1 && condition2 == 1 && condition3 == 1)
-                {
-                    switch (z3)
-                    {
-                    case 1:
-                        printf("ZhangSan speaks the truth.\n");
-                        break;
-                    default:
-                        printf("ZhangSan lies.\n");
-                        break;
-                    }
+        if (condition1 == 1 && condition2 == 1 && condition3 == 1) {
+          switch (z3) {
+            case 1:
+              printf("ZhangSan speaks the truth.\n");
+              break;
+            default:
+              printf("ZhangSan lies.\n");
+              break;
+          }
 
-                    switch (l4)
-                    {
-                    case 1:
-                        printf("LiSi speaks the truth.\n");
-                        break;
-                    default:
-                        printf("Lisi lies.\n");
-                        break;
-                    }
+          switch (l4) {
+            case 1:
+              printf("LiSi speaks the truth.\n");
+              break;
+            default:
+              printf("Lisi lies.\n");
+              break;
+          }
 
-                    switch (w5)
-                    {
-                    case 1:
-                        printf("WangWU speaks the truth.\n");
-                        break;
-                    default:
-                        printf("WangWU lies.\n");
-                        break;
-                    }
-                }
-            }
+          switch (w5) {
+            case 1:
+              printf("WangWU speaks the truth.\n");
+              break;
+            default:
+              printf("WangWU lies.\n");
+              break;
+          }
         }
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
